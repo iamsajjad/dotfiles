84aa8e030f59346001b4dace7669693520aec584
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-03-12 02:22:00 +0300

    Neovim : rewrite help.lua with lua api
    - rewrite `help.lua` autocommands with lua api for
    autocommand and augroup support via `vim.api` using two methods
    `vim.api.nvim_create_autocmd` and `vim.api.nvim_create_augroup`
    to create autocommand and augroup.

--- .config/nvim/lua/neovim/commands/help.lua
+++ .config/nvim/lua/neovim/commands/help.lua
@@ -2,19 +2,18 @@
 -- ... help.lua
 
 local api = vim.api
-local cmd = api.nvim_command
 
-local autwo = require("neovim.core.autwo")
+local help = api.nvim_create_augroup("Help", { clear = true })
 
 -- open help vertically and press q to exit
-autwo.BufEnter = {
-  "*.txt",
-  function()
+api.nvim_create_autocmd("BufEnter", {
+  group = help,
+  pattern = "*.txt",
+  callback = function()
     if vim.bo.buftype == "help" then
-      cmd("wincmd L")
-      local handle = api.nvim_get_current_buf()
-      api.nvim_buf_set_keymap(handle, "n", "q", ":q<CR>", { noremap = true, silent = true })
+      api.nvim_command("wincmd L")
+      api.nvim_buf_set_keymap(0, "n", "q", "<CMD>q<CR>", { noremap = true })
     end
   end,
-}
+})
 
