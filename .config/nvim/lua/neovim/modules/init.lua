f7f67d14f1bbf033e729d7c06934f348cfe1b8be
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-03-15 02:30:00 +0300

    Neovim : create modules directory in neovim config
    - create `modules` directory inside `neovim` config directory
    to hold lua modules for neovim configurations.

---
+++ .config/nvim/lua/neovim/modules/init.lua
@@ -0,0 +1,16 @@
+
+-- ... init.lua
+
+local directory = "neovim.modules" .. "."
+
+local modules = {
+  -- add modules here
+}
+
+for _, module in ipairs(modules) do
+  local ok, err = pcall(require, directory .. module)
+  if not ok then
+    error("Error loading " .. module .. "\n\n" .. err)
+  end
+end
+
