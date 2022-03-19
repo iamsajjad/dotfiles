da6f1dd9beda78866e44b34803138480ffd4d713
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-03-20 02:07:00 +0300

    Neovim : migrate nightfox to options table
    - migrate `nightfox` to options table with the following changes
    move `transparent`, `terminal_color`, `styles`, `inverse` under
    options table and move `alt_nc` to be `options.dim_inactive`.
    
    - add `compile_file_suffix` to options table and set the value
    of compiled file suffix to `_compiled`.
    
    - move `colors` to `pallets` concept and `hlgroups` to `groups`
    concept.
    
    - replace `nightfox.load()` function and `fox` keyword with
    default builtin command `colorscheme`.

--- .config/nvim/lua/neovim/plugins/config/interface/nightfox/init.lua
+++ .config/nvim/lua/neovim/plugins/config/interface/nightfox/init.lua
@@ -1,29 +1,33 @@
 
 -- ... init.lua
 
+local cmd = vim.cmd
 local nightfox = require("nightfox")
 
--- this function set the configuration of nightfox. if a value is not passed in the setup function
--- it will be taken from the default configuration above
-nightfox.setup({
-  -- change the colorscheme to use nordfox
-  fox = "nightfox",
-  -- disable setting the background color
+-- set the nightfox options in options table. if a value is not passed in the options table
+-- it will be taken from the default configurations
+local options = {
+  -- compiled file's destination location
+  -- compile_path = utilities.join_paths(vim.fn.stdpath("cache"), "nightfox"),
+  -- compiled file suffix
+  compile_file_suffix = "_compiled",
+  -- disable setting background
   transparent = false,
-  -- non current window bg to alt color see `hl-normalnc`
-  alt_nc = false,
-  -- configure the colors used when opening :terminal
+  -- set terminal colors (vim.g.terminal_color_*)
   terminal_colors = true,
+  -- non focused panes set to alternative background
+  dim_inactive = false,
+  -- styles config
   styles = {
-    -- style that is applied to comments: see `highlight-args` for options
+    -- style that is applied to comments
     comments = "italic",
-    -- style that is applied to functions: see `highlight-args` for options
+    -- style that is applied to functions
     functions = "none",
-    -- style that is applied to keywords: see `highlight-args` for options
+    -- style that is applied to keywords
     keywords = "none",
-    -- style that is applied to strings: see `highlight-args` for options
+    -- style that is applied to strings
     strings = "none",
-    -- style that is applied to variables: see `highlight-args` for options
+    -- style that is applied to variables
     variables = "none",
   },
   inverse = {
@@ -34,20 +38,98 @@ nightfox.setup({
     -- enable/disable inverse highlights for search highlights
     search = false,
   },
-  -- override default colors
-  colors = {
-    -- override the red color for max power
-    -- red = "#ff0000",
-    -- bg_alt = "#000000",
-  },
-  -- override highlight groups
-  hlgroups = {
-    -- override a highlight group with the color red
-    -- tspunctdelimiter = { fg = "${red}" },
-    -- lspcodelens = { bg = "#000000", style = "italic" },
-  },
-})
+  -- list of various plugins and additional options
+  modules = {},
+}
 
--- load the configuration set above and apply the colorscheme
--- nightfox.load()
+-- pallets are the base color defines of a colorscheme.
+-- you can override these pallets for each colorscheme defined by nightfox
+local pallets = {
+  nightfox = {
+    -- each pallet defines these colors:
+    --   black, red, green, yellow, blue, magenta, cyan, white, orange, pink
+    --
+    -- these colors have 3 shades: base, bright, and dim
+    --
+    -- defining just a color defines it's base color
+    -- red = "#c94f6d",
+
+    -- defining multiple shades is done by passing a table
+    -- blue = { base = "#4d688e", bright = "#4e75aa", dim = "#485e7d", }
+
+    -- a pallet also defines the following:
+    --   bg0, bg1, bg2, bg3, bg4, fg0, fg1, fg2, fg3, sel0, sel1, comment
+    --
+    -- these are the different foreground and background shades used by the theme.
+    -- the base bg and fg is 1, 0 is normally the dark alternative. the others are
+    -- incrementally lighter versions.
+    -- bg1 = "#2e3440",
+
+    -- sel is different types of selection colors.
+    -- sel0 = "#3e4a5b", -- popup bg, visual selection bg
+    -- sel1 = "#4f6074", -- popup sel bg, search bg
+
+    -- comment is the definition of the comment color.
+    -- comment = "#60728a",
+  },
+  dayfox = {
+    -- pallets config for dayfox
+  },
+  dawnfox = {
+    -- pallets config for dawnfox
+  },
+  duskfox = {
+    -- pallets config for duskfox
+  },
+  nordfox = {
+    -- pallets config for nordfox
+  },
+}
+
+-- spec's (specifications) are a mapping of pallets to logical groups that will be
+-- used by the groups. some examples of the groups that specs map would be:
+--   - syntax groups (functions, types, keywords, ...)
+--   - diagnostic groups (error, warning, info, hints)
+--   - git groups (add, removed, changed)
+
+-- you can override these just like pallets
+local specs = {
+  nightfox = {
+    syntax = {
+      -- specs allow you to define a value using either a color or template. if the string does
+      -- start with `#` the string will be used as the path of the pallet table. defining just
+      -- a color uses the base version of that color.
+      -- keyword = "magenta",
+
+      -- adding either `.bright` or `.dim` will change the value
+      -- conditional = "magenta.bright",
+      -- number = "orange.dim",
+    },
+    git = {
+      -- a color define can also be used
+      -- changed = "#f4a261",
+    },
+  },
+}
+
+-- groups are the highlight group definitions. the keys of this table are the name of the highlight
+-- groups that will be overridden. the value is a table with the following values:
+--   - fg, bg, style, sp, link,
+--
+-- just like `spec` groups support templates. this time the template is based on a spec object.
+local groups = {
+  -- by default nightfox links some groups together. `cursorline` is one of these groups. when overriding
+  -- make sure `link` is cleared to `""` so that the link will be removed.
+  -- cursorline = { bg = "sel0", link = "" },
+
+  -- specs are used for the template. specs have their pallet's as a field that can be accessed
+  -- incsearch = { bg = "pallet.cyan" },
+}
+
+-- load nightfox ( options, pallets, specs, groups ) configurations
+nightfox.setup({ options = options, pallets = pallets, specs = specs, groups = groups })
+
+-- apply one of nightfox colorscheme ( nightfox, dayfox, dawnfox, duskfox, nordfox )
+-- with ( options, pallets, specs, groups ) configurations above
+-- cmd("colorscheme nightfox")
 
