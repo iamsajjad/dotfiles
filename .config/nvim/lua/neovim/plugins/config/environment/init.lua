67153c8e05c46fb69c8d5f13e94ac6cec0947408
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-04 02:22:00 +0300

    Neovim : add plugins config environment module
    - add plugins config environment module under plugins config
    namespace to be loaded with `neovim.plugins.config.environment`
    introduced in `init.lua` file under `environment` directory
    to hold configurations for `environment` namespace that need
    to be loaded after the configurations of each plugin under
    `environment` namespace is loaded.

---
+++ .config/nvim/lua/neovim/plugins/config/environment/init.lua
@@ -0,0 +1,15 @@
+
+-- ... init.lua
+
+-- hold configurations for `environment` namespace that need to be
+-- loaded after the configurations of each plugin under `environment`
+-- namespace is loaded
+
+local Environment = {}
+
+function Environment.configure()
+  -- configure environment plugins here.
+end
+
+return Environment
+
