047f29ac4c0209758aad10b1a4e903f7122f6351
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-04 02:12:00 +0300

    Neovim : add plugins config code module
    - add plugins config code module under plugins config
    namespace to be loaded with `neovim.plugins.config.code`
    introduced in `init.lua` file under `code` directory to hold
    configurations for `code` namespace that need to be loaded
    after the configurations of each plugin under `code`
    namespace is loaded.

---
+++ .config/nvim/lua/neovim/plugins/config/code/init.lua
@@ -0,0 +1,15 @@
+
+-- ... init.lua
+
+-- hold configurations for `code` namespace that need to be
+-- loaded after the configurations of each plugin under `code`
+-- namespace is loaded
+
+local Code = {}
+
+function Code.configure()
+  -- configure code plugins here.
+end
+
+return Code
+
