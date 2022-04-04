1642e76531c8b90ddd6a4d98fe2be43526c9b991
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-05 02:14:00 +0300

    Neovim : add plugins config interface module
    - add plugins config interface module under plugins config
    namespace to be loaded with `neovim.plugins.config.interface`
    introduced in `init.lua` file under `interface` directory
    to hold configurations for `interface` namespace that need
    to be loaded after the configurations of each plugin under
    `interface` namespace is loaded.

---
+++ .config/nvim/lua/neovim/plugins/config/interface/init.lua
@@ -0,0 +1,15 @@
+
+-- ... init.lua
+
+-- hold configurations for `interface` namespace that need to be
+-- loaded after the configurations of each plugin under `interface`
+-- namespace is loaded
+
+local Interface = {}
+
+function Interface.configure()
+  -- configure interface plugins here.
+end
+
+return Interface
+
