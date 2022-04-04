b195c4aaa2b58d1cc621d55d49e9e82849271e39
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-05 02:24:00 +0300

    Neovim : add plugins config lsp module
    - add plugins config lsp module under plugins config
    namespace to be loaded with `neovim.plugins.config.lsp`
    introduced in `init.lua` file under `lsp` directory to hold
    configurations for `lsp` namespace that need to be loaded
    after the configurations of each plugin under `lsp`
    namespace is loaded.

---
+++ .config/nvim/lua/neovim/plugins/config/lsp/init.lua
@@ -0,0 +1,15 @@
+
+-- ... init.lua
+
+-- hold configurations for `lsp` namespace that need to be
+-- loaded after the configurations of each plugin under `lsp`
+-- namespace is loaded
+
+local Lsp = {}
+
+function Lsp.configure()
+  -- configure lsp plugins here.
+end
+
+return Lsp
+
