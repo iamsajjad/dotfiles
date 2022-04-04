9b1b123f3cb596425ca496005263ca3003fc751e
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-05 02:04:00 +0300

    Neovim : add plugins config integration module
    - add plugins config integration module under plugins config
    namespace to be loaded with `neovim.plugins.config.integration`
    introduced in `init.lua` file under `integration` directory
    to hold configurations for `integration` namespace that need
    to be loaded after the configurations of each plugin under
    `integration` namespace is loaded.

---
+++ .config/nvim/lua/neovim/plugins/config/integration/init.lua
@@ -0,0 +1,15 @@
+
+-- ... init.lua
+
+-- hold configurations for `integration` namespace that need to be
+-- loaded after the configurations of each plugin under `integration`
+-- namespace is loaded
+
+local Integration = {}
+
+function Integration.configure()
+  -- configure integration plugins here.
+end
+
+return Integration
+
