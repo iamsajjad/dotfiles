0b0a37472ffac56eec4d3baed2caf9f5d452ae84
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-06 02:27:00 +0300

    Neovim : add colors method to interface class
    - add colors method to interface class to provide a wrapper
    around `colorscheme` method to set or change colorscheme
    after configurations of colorscheme plugins is loaded.

--- .config/nvim/lua/neovim/plugins/config/interface/init.lua
+++ .config/nvim/lua/neovim/plugins/config/interface/init.lua
@@ -5,10 +5,17 @@
 -- loaded after the configurations of each plugin under `interface`
 -- namespace is loaded
 
+local api = vim.api
+
 local Interface = {}
 
 function Interface.configure()
   -- configure interface plugins here.
+end
+
+function Interface.colors(name)
+  -- set or change interface colorscheme
+  api.nvim_command(("colorscheme %s"):format(name))
 end
 
 return Interface
