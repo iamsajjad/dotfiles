e3b79b9ab33bc76a0228f4a2315f63147966e0fe
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-06 02:17:00 +0300

    Neovim : add plugins config terminal module
    - add plugins config terminal module under plugins config
    namespace to be loaded with `neovim.plugins.config.terminal`
    introduced in `init.lua` file under `terminal` directory
    to hold configurations for `terminal` namespace that need
    to be loaded after the configurations of each plugin under
    `terminal` namespace is loaded.

---
+++ .config/nvim/lua/neovim/plugins/config/terminal/init.lua
@@ -0,0 +1,15 @@
+
+-- ... init.lua
+
+-- hold configurations for `terminal` namespace that need to be
+-- loaded after the configurations of each plugin under `terminal`
+-- namespace is loaded
+
+local Terminal = {}
+
+function Terminal.configure()
+  -- configure terminal plugins here.
+end
+
+return Terminal
+
