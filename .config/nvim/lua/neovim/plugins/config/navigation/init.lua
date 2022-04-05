69c130b224a2c09646e01534f5d6bd5ed33a87a6
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-06 02:07:00 +0300

    Neovim : add plugins config navigation module
    - add plugins config navigation module under plugins config
    namespace to be loaded with `neovim.plugins.config.navigation`
    introduced in `init.lua` file under `navigation` directory
    to hold configurations for `navigation` namespace that need
    to be loaded after the configurations of each plugin under
    `navigation` namespace is loaded.

---
+++ .config/nvim/lua/neovim/plugins/config/navigation/init.lua
@@ -0,0 +1,15 @@
+
+-- ... init.lua
+
+-- hold configurations for `navigation` namespace that need to be
+-- loaded after the configurations of each plugin under `navigation`
+-- namespace is loaded
+
+local Navigation = {}
+
+function Navigation.configure()
+  -- configure navigation plugins here.
+end
+
+return Navigation
+
