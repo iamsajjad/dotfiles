3f1394d6c220c32cf59fc4952fc36a89d9c9f350
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-10 02:26:00 +0300

    Neovim : add default config for colorizer plugin
    - add default config for `colorizer` plugin configurations
    module or file, through add default configurations options
    with comments to explain the options to know what is possible
    through manipulate available configurations options.

--- .config/nvim/lua/neovim/plugins/config/interface/colorizer/init.lua
+++ .config/nvim/lua/neovim/plugins/config/interface/colorizer/init.lua
@@ -1,5 +1,47 @@
 
 -- ... init.lua
 
-require("colorizer").setup()
+local colorizer = require("colorizer")
+
+local config = {
+
+  --[[
+
+  -- default options can be replace with file type
+  DEFAULT_OPTIONS = {
+
+    -- #RGB hex codes
+    RGB = true,
+
+    -- #RRGGBB hex codes
+    RRGGBB = true,
+
+    -- "Name" codes like Blue
+    names = true,
+
+    -- #RRGGBBAA hex codes
+    RRGGBBAA = false,
+
+    -- CSS rgb() and rgba() functions
+    rgb_fn = false,
+
+    -- CSS hsl() and hsla() functions
+    hsl_fn = false,
+
+    -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+    css = false,
+
+    -- Enable all CSS *functions*: rgb_fn, hsl_fn
+    css_fn = false,
+
+    -- Available modes: foreground, background
+    -- Set the display mode.
+    mode = "background",
+  },
+
+  --]]
+}
+
+-- load colorizer configurations
+colorizer.setup(config)
 
