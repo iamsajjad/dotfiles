7b095ad6054369445c3cf4f44d2be7049e37242c
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-11 02:00:00 +0300

    Neovim : add default config for devicons plugin
    - add default config for `devicons` plugin configurations
    module or file, through add default configurations options
    with comments to explain the options to know what is possible
    through manipulate available configurations options.

--- .config/nvim/lua/neovim/plugins/config/interface/devicons/init.lua
+++ .config/nvim/lua/neovim/plugins/config/interface/devicons/init.lua
@@ -1,5 +1,28 @@
 
 -- ... init.lua
 
-require("nvim-web-devicons").setup()
+local devicons = require("nvim-web-devicons")
+
+local config = {
+
+  -- your personnal icons can go here (to override)
+  -- you can specify color or cterm_color instead of specifying both of them
+  -- devicon will be appended to `name`
+  override = {
+    -- devicon for zsh files
+    -- zsh = {
+    --   icon = "",
+    --   color = "#428850",
+    --   cterm_color = "65",
+    --   name = "Zsh"
+    -- },
+  },
+
+  -- globally enable default icons (default to false)
+  -- will get overriden by `get_icons` option
+  default = true,
+}
+
+-- load colorizer configurations
+devicons.setup(config)
 
