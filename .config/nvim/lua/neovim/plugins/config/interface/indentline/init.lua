c9ab9e33444faa8a7f1374ccabc2fc2fccc2c616
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-11 02:10:00 +0300

    Neovim : add default config for indentline plugin
    - add default config for `indentline` plugin configurations
    module or file, through add default configurations options
    with comments to explain the options to know what is possible
    through manipulate available configurations options.

--- .config/nvim/lua/neovim/plugins/config/interface/indentline/init.lua
+++ .config/nvim/lua/neovim/plugins/config/interface/indentline/init.lua
@@ -1,9 +1,56 @@
 
 -- ... init.lua
 
-require("indent_blankline").setup({
+local indentline = require("indent_blankline")
+
+local config = {
+
+  -- char = "│",
+
+  -- space_char_blankline = " ",
+
   show_first_indent_level = false,
-  filetype_exclude = { "help", "packer", "FTerm" },
-  buftype_exclude = { "terminal", "nofile" },
-})
+
+  -- show_trailing_blankline_indent = false,
+
+  -- file type exclude
+  filetype_exclude = {
+    "help",
+    "packer",
+    "FTerm",
+  },
+
+  -- buffer type exclude
+  buftype_exclude = {
+    "terminal",
+    "nofile",
+  },
+
+  --[[
+
+  -- character highlight
+  char_highlight_list = {
+
+    -- name of highlight group number one
+    "",
+
+    -- name of highlight group number two
+    "",
+  },
+
+  -- space between characters highlight
+  space_char_highlight_list = {
+
+    -- name of highlight group number one
+    "",
+
+    -- name of highlight group number two
+    "",
+  },
+
+  --]]
+}
+
+-- load indentline configurations
+indentline.setup(config)
 
