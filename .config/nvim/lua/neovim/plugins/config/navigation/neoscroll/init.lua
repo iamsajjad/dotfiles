007a45fa24f982d35731073d7a55896207696de8
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-12 02:12:00 +0300

    Neovim : add default config for neoscroll plugin
    - add default config for `neoscroll` plugin configurations
    module or file, through add default configurations options
    with comments to explain the options to know what is possible
    through manipulate available configurations options.

--- .config/nvim/lua/neovim/plugins/config/navigation/neoscroll/init.lua
+++ .config/nvim/lua/neovim/plugins/config/navigation/neoscroll/init.lua
@@ -1,7 +1,51 @@
 
 -- ... init.lua
 
-require("neoscroll").setup({
+local neoscroll = require("neoscroll")
+
+local config = {
+
+  -- all these keys will be mapped to their corresponding default scrolling animation
+  mappings = {
+    "<C-u>",
+    "<C-d>",
+    "<C-b>",
+    "<C-f>",
+    "<C-y>",
+    "<C-e>",
+    "zt",
+    "zz",
+    "zb",
+  },
+
+  -- hide cursor while scrolling
   hide_cursor = false,
-})
+
+  -- stop at <EOF> when scrolling downwards
+  stop_eof = true,
+
+  -- use the local scope of scrolloff instead of the global scope
+  use_local_scrolloff = false,
+
+  -- stop scrolling when the cursor reaches the scrolloff margin of the file
+  respect_scrolloff = false,
+
+  -- the cursor will keep on scrolling even if the window cannot scroll further
+  cursor_scrolls_alone = true,
+
+  -- default easing function
+  easing_function = nil,
+
+  -- function to run before the scrolling animation starts
+  pre_hook = nil,
+
+  -- function to run after the scrolling animation ends
+  post_hook = nil,
+
+  -- disable "performance mode" on all buffers.
+  performance_mode = false,
+}
+
+-- load neoscroll configurations
+neoscroll.setup(config)
 
