f03b6f23c916530ad89fe7cc833ba62178d809cf
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-04-12 02:02:00 +0300

    Neovim : add default config for navigator plugin
    - add default config for `navigator` plugin configurations
    module or file, through add default configurations options
    with comments to explain the options to know what is possible
    through manipulate available configurations options.

--- .config/nvim/lua/neovim/plugins/config/navigation/navigator/init.lua
+++ .config/nvim/lua/neovim/plugins/config/navigation/navigator/init.lua
@@ -3,13 +3,26 @@
 
 local U = require("neovim.core.utilities")
 
-require("Navigator").setup({
+local navigator = require("Navigator")
+
+local config = {
+
+  -- when you want to save the modified buffers when moving to tmux
+  -- nil - Don't save (default)
+  -- 'current' - Only save the current modified buffer
+  -- 'all' - Save all the buffers
   auto_save = "current",
-})
+
+  -- disable navigation when tmux is zoomed in
+  disable_on_zoom = false,
+}
 
 -- bindings
 U.map("n", "<C-h>", '<CMD>lua require("Navigator").left()<CR>')
 U.map("n", "<C-k>", '<CMD>lua require("Navigator").up()<CR>')
 U.map("n", "<C-l>", '<CMD>lua require("Navigator").right()<CR>')
 U.map("n", "<C-j>", '<CMD>lua require("Navigator").down()<CR>')
+
+-- load navigator configurations
+navigator.setup(config)
 
