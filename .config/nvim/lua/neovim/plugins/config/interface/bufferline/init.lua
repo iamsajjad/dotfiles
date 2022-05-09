1f442a858a844183a3aaff7ab041ee8b968569dc
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-05-10 02:18:00 +0300

    Neovim : bufferline allow show buffer close icons
    - `bufferline` allow show buffer close icons for each buffer
    via set `show_buffer_close_icons` to `true`.

--- .config/nvim/lua/neovim/plugins/config/interface/bufferline/init.lua
+++ .config/nvim/lua/neovim/plugins/config/interface/bufferline/init.lua
@@ -106,7 +106,7 @@ local config = {
     -- disable filetype icons for buffers
     show_buffer_icons = true,
 
-    show_buffer_close_icons = false,
+    show_buffer_close_icons = true,
     show_close_icon = false,
     show_tab_indicators = true,
 
