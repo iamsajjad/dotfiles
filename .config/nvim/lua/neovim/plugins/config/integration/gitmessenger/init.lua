c00dc79b8e6d6731672cb0ddbe6ca170af82305d
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-05-24 02:23:00 +0300

    Neovim : switch to mapper for git messenger plugin
    - switch to `mapper.lua` `map` method for setting key map
    via `vim.keymap.set` and delete `utilities.lua` `map` method
    which setting key map via `vim.api.nvim_set_keymap` for
    `git messenger` plugin config.

--- .config/nvim/lua/neovim/plugins/config/integration/gitmessenger/init.lua
+++ .config/nvim/lua/neovim/plugins/config/integration/gitmessenger/init.lua
@@ -1,11 +1,11 @@
 
 -- ... init.lua
 
-local U = require("neovim.core.utilities")
+local Mapper = require("neovim.core.mapper")
 local g = vim.g
 
 g.git_messenger_no_default_mappings = true
 
 -- bindings
-U.map("n", "gm", ":GitMessenger<CR>")
+Mapper.map("n", "gm", ":GitMessenger<CR>")
 
