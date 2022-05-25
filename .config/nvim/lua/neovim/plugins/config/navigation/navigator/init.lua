aeeedb98685afa23bd5acd3ae161bcd4a84d3ca5
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-05-26 02:09:00 +0300

    Neovim : switch to mapper for navigator plugin
    - switch to `mapper.lua` `map` method for setting key map
    via `vim.keymap.set` and delete `utilities.lua` `map` method
    which setting key map via `vim.api.nvim_set_keymap` for
    `navigator` plugin config.

--- .config/nvim/lua/neovim/plugins/config/navigation/navigator/init.lua
+++ .config/nvim/lua/neovim/plugins/config/navigation/navigator/init.lua
@@ -1,7 +1,7 @@
 
 -- ... init.lua
 
-local U = require("neovim.core.utilities")
+local Mapper = require("neovim.core.mapper")
 
 local navigator = require("Navigator")
 
@@ -17,12 +17,12 @@ local config = {
   disable_on_zoom = false,
 }
 
--- bindings
-U.map("n", "<C-h>", '<CMD>lua require("Navigator").left()<CR>')
-U.map("n", "<C-k>", '<CMD>lua require("Navigator").up()<CR>')
-U.map("n", "<C-l>", '<CMD>lua require("Navigator").right()<CR>')
-U.map("n", "<C-j>", '<CMD>lua require("Navigator").down()<CR>')
-
 -- load navigator configurations
 navigator.setup(config)
+
+-- bindings
+Mapper.map("n", "<C-h>", navigator.left)
+Mapper.map("n", "<C-k>", navigator.up)
+Mapper.map("n", "<C-l>", navigator.right)
+Mapper.map("n", "<C-j>", navigator.down)
 
