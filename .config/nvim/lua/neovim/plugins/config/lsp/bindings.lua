0ccdf5e39d87876f1d6f5e38db510d87a3fd4ceb
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-06-22 02:15:00 +0300

    Neovim : add diagnostic bindings to bindings.lua
    - add diagnostic bindings to `bindings.lua` module to handle
    diagnostic actions without map the bindings to specific
    buffer so the bindings attached to available buffer types
    regardless of `lsp` or language server protocol the buffer
    is attached to.

--- .config/nvim/lua/neovim/plugins/config/lsp/bindings.lua
+++ .config/nvim/lua/neovim/plugins/config/lsp/bindings.lua
@@ -1,3 +1,14 @@
 
 -- ... bindings.lua
 
+local Mapper = require("neovim.core.mapper")
+
+-- diagnostic options
+local opts = { noremap = true, silent = true, expr = false }
+
+-- diagnostic bindings
+Mapper.map("n", "<space>e", vim.diagnostic.open_float, opts)
+Mapper.map("n", "[d", vim.diagnostic.goto_prev, opts)
+Mapper.map("n", "]d", vim.diagnostic.goto_next, opts)
+Mapper.map("n", "<space>q", vim.diagnostic.setloclist, opts)
+
