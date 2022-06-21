fddde6e78189a4c29da341bce03767167ffcc583
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-06-22 02:25:00 +0300

    Neovim : add lsp bindings to bindings.lua
    - add `lsp` or language server protocol to `bindings.lua`
    module to handle `lsp` or language server protocol actions
    with map the bindings to specific buffer so the bindings
    attached to the language server protocol buffer through
    buffer specific number.

--- .config/nvim/lua/neovim/plugins/config/lsp/bindings.lua
+++ .config/nvim/lua/neovim/plugins/config/lsp/bindings.lua
@@ -3,6 +3,8 @@
 
 local Mapper = require("neovim.core.mapper")
 
+local Bindings = {}
+
 -- diagnostic options
 local opts = { noremap = true, silent = true, expr = false }
 
@@ -11,4 +13,30 @@ Mapper.map("n", "<space>e", vim.diagnostic.open_float, opts)
 Mapper.map("n", "[d", vim.diagnostic.goto_prev, opts)
 Mapper.map("n", "]d", vim.diagnostic.goto_next, opts)
 Mapper.map("n", "<space>q", vim.diagnostic.setloclist, opts)
+
+-- LSP bindings
+-- @param buffer number
+function Bindings.init(buffer)
+  -- options with specific buffer number
+  opts = { noremap = true, silent = true, expr = false, buffer = buffer }
+
+  -- see `:help vim.lsp.*` for documentation on any of the below functions
+  Mapper.map("n", "gD", vim.lsp.buf.declaration, opts)
+  Mapper.map("n", "gd", vim.lsp.buf.definition, opts)
+  Mapper.map("n", "K", vim.lsp.buf.hover, opts)
+  Mapper.map("n", "gi", vim.lsp.buf.implementation, opts)
+  Mapper.map("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+  Mapper.map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+  Mapper.map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+  Mapper.map("n", "<space>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, opts)
+  Mapper.map("n", "<space>D", vim.lsp.buf.type_definition, opts)
+  Mapper.map("n", "<space>rn", vim.lsp.buf.rename, opts)
+  Mapper.map("n", "<space>ca", vim.lsp.buf.code_action, opts)
+  Mapper.map("n", "gr", vim.lsp.buf.references, opts)
+  Mapper.map("n", "<space>f", vim.lsp.buf.format, opts)
+end
+
+return Bindings
 
