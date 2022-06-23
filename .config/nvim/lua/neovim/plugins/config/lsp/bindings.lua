abbdcea6dc8e30e33846fdca5f6e31736008f49f
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-06-24 02:12:00 +0300

    Neovim : rename diagnostic bindings opts variable
    - rename diagnostic bindings `opts` variable to diagnostic
    specific bindings options holder variable `diagnosticOpts` to
    separate diagnostic options from `lsp` or language server
    protocol options.

--- .config/nvim/lua/neovim/plugins/config/lsp/bindings.lua
+++ .config/nvim/lua/neovim/plugins/config/lsp/bindings.lua
@@ -6,19 +6,19 @@ local Mapper = require("neovim.core.mapper")
 local Bindings = {}
 
 -- diagnostic options
-local opts = { noremap = true, silent = true, expr = false }
+local diagnosticOpts = { noremap = true, silent = true, expr = false }
 
 -- diagnostic bindings
-Mapper.map("n", "<space>e", vim.diagnostic.open_float, opts)
-Mapper.map("n", "[d", vim.diagnostic.goto_prev, opts)
-Mapper.map("n", "]d", vim.diagnostic.goto_next, opts)
-Mapper.map("n", "<space>q", vim.diagnostic.setloclist, opts)
+Mapper.map("n", "<space>e", vim.diagnostic.open_float, diagnosticOpts)
+Mapper.map("n", "[d", vim.diagnostic.goto_prev, diagnosticOpts)
+Mapper.map("n", "]d", vim.diagnostic.goto_next, diagnosticOpts)
+Mapper.map("n", "<space>q", vim.diagnostic.setloclist, diagnosticOpts)
 
 -- LSP bindings
 -- @param buffer number
 function Bindings.init(buffer)
   -- options with specific buffer number
-  opts = { noremap = true, silent = true, expr = false, buffer = buffer }
+  local opts = { noremap = true, silent = true, expr = false, buffer = buffer }
 
   -- see `:help vim.lsp.*` for documentation on any of the below functions
   Mapper.map("n", "gD", vim.lsp.buf.declaration, opts)
