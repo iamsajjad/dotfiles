beeecaf93404d30b9d0c971658f95d2e495cd753
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-06-24 02:02:00 +0300

    Neovim : remove utilities.lua lsp bindings
    - remove `utilities.lua` `lsp` or language server protocol
    bindings after `lsp` or language server protocol bindings
    is begin handled via `bindings.lua` module.

--- .config/nvim/lua/neovim/plugins/config/lsp/utilities.lua
+++ .config/nvim/lua/neovim/plugins/config/lsp/utilities.lua
@@ -1,8 +1,6 @@
 
 -- ... utilities.lua
 
-local Mapper = require("neovim.core.mapper")
-
 local U = {}
 
 local formatting = vim.api.nvim_create_augroup("Formatting", { clear = true })
@@ -31,30 +29,6 @@ function U.capabilities()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
 
   return require("cmp_nvim_lsp").update_capabilities(capabilities)
-end
-
----LSP mappings
----@param buffer number
-function U.mappings(buffer)
-  -- options with specific buffer number
-  local opts = { noremap = true, silent = true, expr = false, buffer = buffer }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  Mapper.map("n", "gD", vim.lsp.buf.declaration, opts)
-  Mapper.map("n", "gd", vim.lsp.buf.definition, opts)
-  Mapper.map("n", "K", vim.lsp.buf.hover, opts)
-  Mapper.map("n", "gi", vim.lsp.buf.implementation, opts)
-  Mapper.map("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-  Mapper.map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-  Mapper.map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-  Mapper.map("n", "<space>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts)
-  Mapper.map("n", "<space>D", vim.lsp.buf.type_definition, opts)
-  Mapper.map("n", "<space>rn", vim.lsp.buf.rename, opts)
-  Mapper.map("n", "<space>ca", vim.lsp.buf.code_action, opts)
-  Mapper.map("n", "gr", vim.lsp.buf.references, opts)
-  Mapper.map("n", "<space>f", vim.lsp.buf.format, opts)
 end
 
 return U
