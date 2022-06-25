76996d5365df13d556414502b42f28f3ad5b0a05
Sajjad alDalwachee <sajjad.aldalwachee@gmail.com>
2022-06-26 02:17:00 +0300

    Neovim : remove nvimtree auto_close option
    - remove `nvimtree` option `auto_close` from `nvimtree`
    `config` table after `auto_close` option is removed
    from `nvimtree` configurations.

--- .config/nvim/lua/neovim/plugins/config/navigation/nvimtree/init.lua
+++ .config/nvim/lua/neovim/plugins/config/navigation/nvimtree/init.lua
@@ -32,9 +32,6 @@ local config = {
   -- will not open on setup if the filetype is in this list
   ignore_ft_on_setup = {},
 
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close = false,
-
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
   open_on_tab = false,
 
